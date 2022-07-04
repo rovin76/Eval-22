f03a6dbc4df64458f6438bff2dff4b52ec05579b
rovin76 <105910714+rovin76@users.noreply.github.com>
2022-07-04 14:30:00 +0530

    commit

--- src/Components/users/Users.jsx
+++ src/Components/users/Users.jsx
@@ -4,13 +4,27 @@ import Filters from "./Filters";
 import UserCard from "./UserCard";
 
 function Users() {
+  const [data, setData] = useState([]);
+  // get Todos
 
+  const getTodos = () => {
+    fetch("fakeFetch")
+      .then((res) => res.json())
+      .then((d) => {
+        setData(d);
+      });
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+  console.log(data);
   return (
     <div>
       <div data-testid="loading-indicator">...loading</div>
       <div data-testid="error-indicator">Error: Something went wrong</div>
       <UserCard />
-      <Filters/>
+      <Filters />
       <Pagination />
     </div>
   );
